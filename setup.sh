a8138e4aa81f8e2dcd1ea20a34a392541252368d
sangamgyawali <61397980+sangamgyawali@users.noreply.github.com>
2020-11-03 12:17:55 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"sangamgyawali0@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.to
